a6ee3f0000b20addb5e8c746c59ae17142b4a695
Derek Weitzel <djw8605@gmail.com>
2019-12-08 20:01:48 -0600

    Test with curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 
 ### Verify Step
 PYTHONPATH=src/ mkdocs serve --verbose --strict &
-sleep 10
-timeout 30 bash -c 'until (echo > /dev/tcp/localhost/8000); do sleep 0.5; done' > /dev/null 2>&1
+timeout 30 bash -c 'until (curl http://localhost:8000/); do sleep 0.5; done' 
+# > /dev/null 2>&1
 
 if `which linkchecker &>/dev/null`; then
     timeout 600 bash -c 'linkchecker --config=ci/linkchecker.config http://localhost:8000/'
