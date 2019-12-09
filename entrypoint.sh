9795a600db1adb7e88800db27ff6bc704d4ba356
Derek Weitzel <djw8605@gmail.com>
2019-12-08 19:56:19 -0600

    Add sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 
 ### Verify Step
 PYTHONPATH=src/ mkdocs serve --verbose --strict &
+sleep 10
 timeout 30 bash -c 'until (echo > /dev/tcp/localhost/8000); do sleep 0.5; done' > /dev/null 2>&1
 
 if `which linkchecker &>/dev/null`; then
